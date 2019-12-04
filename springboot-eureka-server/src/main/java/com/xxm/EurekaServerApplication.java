fa23bec4c3f6b0987d5af4d689e3458436ac5eb3
SOFTRE <39897273+SOFTRE@users.noreply.github.com>
2019-12-04 19:55:09 +0800

    one demo

---
+++ springboot-eureka-server/src/main/java/com/xxm/EurekaServerApplication.java
@@ -0,0 +1,22 @@
+package com.xxm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * 描述
+ *
+ * @author Mr Liu
+ * @version 1.0
+ * @package com.xxm *
+ * @Date 2019-11-22
+ * @since 1.0
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+}
